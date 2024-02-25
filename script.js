062ceed6e0b4cd97b2c8bc99e4ea316449d50b60
Jeff Ndegwa <ndegwajeff4@gmail.com>
2024-02-25 11:29:53 +0300

    Introduction to javascript for begginers

---
+++ script.js
@@ -0,0 +1,36 @@
+// Declare a variable using the "let" keyword
+let myNumber;
+
+// Assign a value to the variable
+myNumber = 42;
+
+// Log the value to the console
+console.log("The value of myNumber is:", myNumber);
+
+// Declare and assign a variable in one line
+let myString = 'Hello, World!';
+
+// Log the string to the console
+console.log("The value of myString is:", myString);
+
+// Change the value of a variable
+myNumber = 100;
+
+// Log the updated value to the console
+console.log("The updated value of myNumber is:", myNumber);
+
+// Avoid declaring a variable multiple times
+let myVariable = 'Initial Value';
+console.log("Original value of myVariable:", myVariable);
+
+// Trying to redeclare the variable will result in an error
+// let myVariable = 'New Value'; // Uncommenting this line will cause an error
+
+// Best practices for variable naming
+let userName = 'JohnDoe';
+let itemCount = 5;
+
+// Summary
+console.log("Summary:");
+console.log("Variable userName:", userName);
+console.log("Variable itemCount:", itemCount);
